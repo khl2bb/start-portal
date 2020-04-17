bfbb497371c83497c263a3e21632859b34f9b505
이진우 <khl2bb@naver.com>
2020-04-17 21:19:26 +0900

    (3)-1 insert 및 refactor
    insert 테스트
    refactor 도입
    
    mysql insert 가 실제로 되는 걸보니 즐겁습니다.

--- userdao11/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao11/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -6,10 +6,7 @@ public class UserDao {
     public User get(Integer id) throws ClassNotFoundException, SQLException {
         //mysql
         //driver 로딩
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        //connection
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul"
-                , "jeju", "jejupw");
+        Connection connection = getConnection();
         //query
         PreparedStatement preparedStatement =
                 connection.prepareStatement("select id, name, password from userinfo where id = ?");
@@ -28,5 +25,33 @@ public class UserDao {
         connection.close();
         //결과리턴
         return user;
+    }
+
+    public void insert(User user) throws ClassNotFoundException, SQLException {
+        //mysql
+        //driver 로딩
+        Connection connection = getConnection();
+        //query
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("insert into userinfo (name, password) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.executeUpdate();
+        //실행
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+        //결과매핑
+        user.setId(resultSet.getInt(1));
+        //자원해지
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //connection
+        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul"
+                , "jeju", "jejupw");
     }
 }
