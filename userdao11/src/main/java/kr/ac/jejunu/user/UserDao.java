df99724d888643f599b112086b0e0cf24eb7c048
이진우 <khl2bb@naver.com>
2020-04-17 21:50:14 +0900

    (3)-2 hallaDao, abstract
    테스트 할 때 저는
    4 actionable tasks: 3 executed, 1 up-to-date 인데
    교수님은 4 actionable tasks: 4 executed 이네요
    이거 빼고는 문제 없습니다.

--- userdao11/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao11/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -2,7 +2,7 @@ package kr.ac.jejunu.user;
 
 import java.sql.*;
 
-public class UserDao {
+public abstract class UserDao {
     public User get(Integer id) throws ClassNotFoundException, SQLException {
         //mysql
         //driver 로딩
@@ -48,10 +48,10 @@ public class UserDao {
         connection.close();
     }
 
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        //connection
-        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul"
-                , "jeju", "jejupw");
-    }
+    abstract public Connection getConnection() throws ClassNotFoundException, SQLException;
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        //connection
+//        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul"
+//                , "jeju", "jejupw");
+
 }
