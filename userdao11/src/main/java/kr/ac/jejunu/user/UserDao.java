347f5ff740df7db256dab07ac75c2ae77bad0fb6
이진우 <khl2bb@naver.com>
2020-04-25 15:31:22 +0900

    (4)-3 예상되는 문제점 해결
    자원해지가 앞 문장의 exception 처리 때문에 해지가 잘 안되고 넘어가면 나중에 문제로 터지니까
    try finally와 try catch로 적절히 감싼다.

--- userdao11/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao11/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -11,47 +11,82 @@ public class UserDao {
     }
 
     public User get(Integer id) throws ClassNotFoundException, SQLException {
-        //mysql
-        //driver 로딩
-        Connection connection = dataSource.getConnection();
-        //query
-        PreparedStatement preparedStatement =
-                connection.prepareStatement("select id, name, password from userinfo where id = ?");
-        preparedStatement.setInt(1, id);
-        //실행
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-        //결과매핑
-        User user = new User();
-        user.setId(resultSet.getInt("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
-        //자원해지
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        User user;
+        try { // ctrl + alt + T 하면 surround with 단축키
+            //mysql
+            //driver 로딩
+            connection = dataSource.getConnection();
+            //query
+            preparedStatement = connection.prepareStatement("select id, name, password from userinfo where id = ?");
+            preparedStatement.setInt(1, id);
+            //실행
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            //결과매핑
+            user = new User();
+            user.setId(resultSet.getInt("id"));
+            user.setName(resultSet.getString("name"));
+            user.setPassword(resultSet.getString("password"));
+        } finally {
+            //자원해지
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                preparedStatement.close(); // 또 여기서 exception 뜨면 throw때문에 뒤에문장이 실행안될수있으니 흘려보내기
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close(); // 커넥션 클로즈를 안하면 쌓여서 터짐 문제는 exception 던지고 끝나버리니까 close를 못함 그래서 try finally
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         //결과리턴
         return user;
     }
 
-    public void insert(User user) throws ClassNotFoundException, SQLException {
-        //mysql
-        //driver 로딩
-        Connection connection = dataSource.getConnection();
-        //query
-        PreparedStatement preparedStatement =
-                connection.prepareStatement("insert into userinfo (name, password) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
-        preparedStatement.setString(1, user.getName());
-        preparedStatement.setString(2, user.getPassword());
-        preparedStatement.executeUpdate();
-        //실행
-        ResultSet resultSet = preparedStatement.getGeneratedKeys();
-        resultSet.next();
-        //결과매핑
-        user.setId(resultSet.getInt(1));
-        //자원해지
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+    public void insert(User user) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            //mysql
+            //driver 로딩
+            connection = dataSource.getConnection();
+            //query
+            preparedStatement = connection.prepareStatement("insert into userinfo (name, password) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.executeUpdate();
+            //실행
+            resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
+            //결과매핑
+            user.setId(resultSet.getInt(1));
+        } finally {
+            //자원해지
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
