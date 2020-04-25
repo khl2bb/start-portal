94e3038283a998e2bf57f059b8b2055bfda21dc6
이진우 <khl2bb@naver.com>
2020-04-25 13:45:59 +0900

    (4)-1 스프링 bean configuration
    (4)-1 스프링 bean configuration
    gradle 업데이트

--- userdao11/src/main/java/kr/ac/jejunu/user/DaoFactory.java
+++ userdao11/src/main/java/kr/ac/jejunu/user/DaoFactory.java
@@ -1,11 +1,20 @@
 package kr.ac.jejunu.user;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// bean을 정의하는 곳
+@Configuration
 public class DaoFactory {
-    public UserDao getUserDao() {
-        return new UserDao(getConnectionMaker());
+    // 오브젝트 디펜덴시를 받아서 new를 해주는 것
+    // 스프링 bean은 스프링이 new 해주는 오브젝트 인스턴스를 bean이라 한다.
+    @Bean
+    public UserDao userDao() {
+        return new UserDao(connectionMaker());
     }
 
-    private JejuConnectionMaker getConnectionMaker() {
+    @Bean
+    public ConnectionMaker connectionMaker() {
         return new JejuConnectionMaker();
     }
 }
