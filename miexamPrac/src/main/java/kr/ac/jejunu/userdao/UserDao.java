d15caad936cdbbc690fc1477503c6013dd443ff7
이진우 <khl2bb@naver.com>
2020-05-12 04:08:05 +0900

    중간 1. 사용자 추가기능 만들기
    (extract method)
    
    UserDao.java에
    insert() 메소드를 만들고
    테스트를 수행해 보았습니다.

---
+++ miexamPrac/src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -0,0 +1,48 @@
+package kr.ac.jejunu.userdao;
+
+import java.sql.*;
+
+public class UserDao {
+    public User get(Integer id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection =
+                DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=UTC"
+                , "jeju", "jejupw");
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("select * from userinfo where id = ?");
+        preparedStatement.setLong(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        //리턴
+        return user;
+    }
+
+    public void insert(User user) throws ClassNotFoundException, SQLException { //사용자 추가기능
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection =
+                DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=UTC"
+                        , "jeju", "jejupw");
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("insert into userinfo (name, password) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.executeUpdate();
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+        user.setId(resultSet.getInt(1));
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+    }
+}
---
+++ miexamPrac/src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -0,0 +1,42 @@
+package kr.ac.jejunu.userdao;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.core.Is.is;
+
+public class UserDaoTests {
+    @Test
+    public void testGet() throws SQLException, ClassNotFoundException {
+        Integer id = 1;
+        String name = "jinoo";
+        String password = "1234";
+
+        UserDao userDao = new UserDao();
+        User user = userDao.get(id);
+        assertThat(user.getId(), is(id));
+        assertThat(user.getName(), is(name));
+        assertThat(user.getPassword(), is(password));
+    }
+
+    @Test
+    public void testInsert() throws SQLException, ClassNotFoundException {
+        String name = "jinoo";
+        String password = "1234";
+
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+
+        assertThat(user.getId(), greaterThan(0));
+        User insertedUser = userDao.get(user.getId());
+        assertThat(insertedUser.getName(), is(name));
+        assertThat(insertedUser.getPassword(), is(password));
+    }
+}
