bca6a64e2b867469297541bd44d25afa460a8578
이진우 <khl2bb@naver.com>
2020-05-12 22:54:13 +0900

    중간 6. 각자가 만든 Connection 정보관리 Object 를 DataSource 로 변경해보세요.
    DataSource 를 이용하여 커넥션 메이커 수정
    
    +
    환경변수 설정

--- miexamPrac/src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
+++ miexamPrac/src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
@@ -1,19 +1,48 @@
 package kr.ac.jejunu.userdao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
 
 @Configuration //Bean 을정의하는곳
 public class DaoFactory {
 
+    @Value("${db.classname}")
+    private String className;
+    @Value(("${db.url}"))
+    private String url;
+    @Value(("${db.username}"))
+    private String username;
+    @Value(("${db.password}"))
+    private String password;
+
     @Bean // new를 해줘서 디펜덴시를 담아서 인스턴스를 반환하는것, 스프링이 해주는것
     public UserDao userDao() {
-        return new UserDao(connectionMaker());
+        return new UserDao(dataSource());
     }
 
-    @Bean // 스프링이 new 해주는 오브젝트 instance를 bean이라 한다.
-    public ConnectionMaker connectionMaker() {
-        return new JejuConnectionMaker();
+    @Bean
+    public DataSource dataSource() { // connection과 관련된 여러가지 interface를 제공하는 것
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        try {
+            dataSource.setDriverClass((Class<? extends Driver>) Class.forName(className));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+
     }
 
 }
