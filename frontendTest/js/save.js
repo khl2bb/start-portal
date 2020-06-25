2a9a07a609366952d19dbb588ee495eb9e3b5dcb
이진우 <khl2bb@naver.com>
2020-06-26 01:18:58 +0900

    2020/06/25/FRI jQuery, bootstrap 추가 링크 실패
    2020/06/25/FRI jQuery, bootstrap 추가 링크 실패
    js 연결이 잘 안되네요..

--- frontendTest/js/save.js
+++ frontendTest/js/save.js
@@ -3,15 +3,27 @@ const $saveButton = document.getElementById('saveButton');
 function log(str) {
   console.log(str);
 }
-function checkKey(e) {
-  if (e.key === 's') {
-    $saveButton.click();
-  }
-}
-function save() {
-  log(4);
-  $saveButton.style.backgroundColor = black;
-}
 
-window.addEventListener('keypress', checkKey);
-$saveButton.addEventListener('click', save);
+// function checkKey(e) {
+//   if (e.key === 's') {
+//     $saveButton.click();
+//   }
+// }
+// function save() {
+//   log(4);
+//   $saveButton.style.backgroundColor = black;
+// }
+
+$(window).bind('keydown', function (event) {
+  if (event.ctrlKey || event.metaKey) {
+    switch (String.fromCharCode(event.which).toLowerCase()) {
+      case 's':
+        event.preventDefault();
+        $saveButton.click();
+        break;
+    }
+  }
+});
+
+// window.addEventListener('keypress', checkKey);
+// $saveButton.addEventListener('click', save);
