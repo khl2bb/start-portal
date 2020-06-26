e073b51ea973145d949881c8ff489ef655abb754
이진우 <khl2bb@naver.com>
2020-06-26 10:30:04 +0900

    2020/06/25/FRI CTRL + S 단축키 처리 실패
    2020/06/25/FRI CTRL + S 단축키 처리 실패

--- frontendTest/js/save.js
+++ frontendTest/js/save.js
@@ -4,26 +4,29 @@ function log(str) {
   console.log(str);
 }
 
-// function checkKey(e) {
-//   if (e.key === 's') {
-//     $saveButton.click();
-//   }
-// }
-// function save() {
-//   log(4);
-//   $saveButton.style.backgroundColor = black;
-// }
-
-$(window).bind('keydown', function (event) {
-  if (event.ctrlKey || event.metaKey) {
-    switch (String.fromCharCode(event.which).toLowerCase()) {
-      case 's':
-        event.preventDefault();
-        $saveButton.click();
-        break;
-    }
+function checkKey(e) {
+  log(e);
+  if (e.key === 's' && e.ctrlKey) {
+    e.stopPropagation();
+    e.preventDefault();
+    log('S');
   }
-});
+}
+function save() {
+  log(4);
+  $saveButton.style.backgroundColor = black;
+}
 
-// window.addEventListener('keypress', checkKey);
-// $saveButton.addEventListener('click', save);
+// $(window).bind('keydown', function (event) {
+//   if (event.ctrlKey || event.metaKey) {
+//     switch (String.fromCharCode(event.which).toLowerCase()) {
+//       case 's':
+//         event.preventDefault();
+//         $saveButton.click();
+//         break;
+//     }
+//   }
+// });
+
+window.addEventListener('keypress', checkKey);
+$saveButton.addEventListener('click', save);
